2a6cd2823e455b3e74dad3c7b7f04ae754aea637
nandi <xu.n@northeastern.edu>
2022-10-21 12:21:29 -0700

    Completed

--- src/main/java/SkierServlet.java
+++ src/main/java/SkierServlet.java
@@ -1,7 +1,10 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "SkierServlet", value = "/SkierServlet")
 public class SkierServlet extends HttpServlet {
@@ -55,7 +58,7 @@ public class SkierServlet extends HttpServlet {
         // check we have a URL!
         if (urlPath == null || urlPath.isEmpty()) {
             res.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            res.getWriter().write("missing paramterers");
+            res.getWriter().write("missing parameters");
             return;
         }
 
@@ -69,15 +72,64 @@ public class SkierServlet extends HttpServlet {
             res.setStatus(HttpServletResponse.SC_OK);
             // do any sophisticated processing with urlParts which contains all the url params
             // TODO: process url params in `urlParts`
-            String year = urlParts[3];
-            String season = urlParts[1];
-            String day = urlParts[5];
-            String skierID = urlParts[7];
-            res.getWriter().write("This is a POST!");
-            res.getWriter().write("\nYear: " + year);
-            res.getWriter().write("\nSeason: " + season);
-            res.getWriter().write("\nDay: " + day);
-            res.getWriter().write("\nSkierID: " + skierID);
+            BufferedReader reader = req.getReader();
+            String json_material = ReadBigStringIn(reader);
+            HashMap<String, String> skier = ProcessRandomSkierJSON(json_material);
+
+            // print out POST info.
+//            res.getWriter().write("\nThis is a POST");
+//            for (Map.Entry<String, String> entry : skier.entrySet()) {
+//                res.getWriter().write("\n" + entry.getKey() +": " + entry.getValue());
+//            }
+//            res.getWriter().write("\nJSONfile:" + json_material);
         }
     }
+
+    public String ReadBigStringIn(BufferedReader buffIn) throws IOException {
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while( (line = buffIn.readLine()) != null) {
+            everything.append(line);
+        }
+        return everything.toString();
+    }
+
+    public HashMap<String, String> ProcessRandomSkierJSON(String json) {
+        HashMap<String, String> skier = new HashMap<>();
+        String[] json_divided = json.split(", ");
+
+        //remove { and }
+        json_divided[0] = json_divided[0].substring(1);
+        int n = json_divided.length;
+        json_divided[n-1] = json_divided[n-1].substring(0, json_divided[n-1].length() - 1);
+
+
+        for (String s : json_divided) {
+            String[] s_trimed = s.split(":");
+            switch(s_trimed[0]) {
+                case "\"skierID\"":
+                    skier.put("skierID", s_trimed[1]);
+                    break;
+                case "\"resortID\"":
+                    skier.put("resortID", s_trimed[1]);
+                    break;
+                case "\"liftID\"":
+                    skier.put("liftID", s_trimed[1]);
+                    break;
+                case "\"seasonID\"":
+                    skier.put("seasonID", s_trimed[1]);
+                    break;
+                case "\"dayID\"":
+                    skier.put("dayID", s_trimed[1]);
+                    break;
+                case "\"time\"":
+                    skier.put("time", s_trimed[1]);
+                    break;
+            }
+        }
+
+        return skier;
+    }
 }
+
+
